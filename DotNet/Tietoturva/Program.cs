324d62d1cbaab7ff4b1b6211cbb4318da06ca19b
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-03-18 10:08:39 +0200

    Lisätty C#-esimerkki taulukon käsittelystä virheellisellä indeksillä.

--- DotNet/Tietoturva/Program.cs
+++ DotNet/Tietoturva/Program.cs
@@ -7,6 +7,15 @@ namespace Tietoturva
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            char[] merkit = {'a','b','c'};
+            Console.WriteLine("Taulukko alustettu.");
+
+            Console.WriteLine("Merkki 1: "+merkit[0]);
+            Console.WriteLine("Merkki 2: "+merkit[1]);
+            Console.WriteLine("Merkki 3: "+merkit[2]);
+
+            Console.WriteLine("Merkki 4: "+merkit[3]);
         }
     }
 }
