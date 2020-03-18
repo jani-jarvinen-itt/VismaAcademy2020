06db85eded689675daccfa91396ae23547bd3178
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-03-18 11:16:54 +0200

    Lisätty C#-esimerkki SQL-injektiohyökkäyksestä.

---
+++ DotNet/SQL-testi/SqlServerTesti/SqlServerTesti/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlServerTesti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=localhost\\SQLEXPRESS;Database=Northwind;Integrated Security=true;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            MessageBox.Show("Tietokantayhteys avattu.");
+
+            // VAROITUS: SQL-injektiohyökkäysmahdollisuus
+            string maa = textBox1.Text;
+            string sql = "SELECT * FROM Customers WHERE Country = '"+maa+"'";
+            MessageBox.Show(sql);
+
+            SqlCommand command = new SqlCommand(sql, connection);
+            // command.ExecuteReader();
+        }
+    }
+}
---
+++ Tietoturva/Tietoturva.js
@@ -0,0 +1,11 @@
+console.log("Sovellus käynnistyy.");
+
+let luvut = [1,2,3];
+
+console.log("Luku 1: "+luvut[0]);
+console.log("Luku 2: "+luvut[1]);
+console.log("Luku 3: "+luvut[2]);
+
+console.log("Luku 4: "+luvut[3]);
+
+console.log("Sovelluksen suoritus päättyy.");
