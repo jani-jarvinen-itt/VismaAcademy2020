4e62f08858a24ff1bfc45fe34ce733e9696fc53b
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-03-18 09:57:33 +0200

    Lisätty C#-esimerkkisovellus tietoturvaan liittyen.

---
+++ DotNet/Tietoturva/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tietoturva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
