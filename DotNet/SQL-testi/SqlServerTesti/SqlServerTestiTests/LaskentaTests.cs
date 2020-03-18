c33a5434b26ecadb055b37f2aa768d2cf5f025c4
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-03-18 13:51:59 +0200

    Lisätty sekä .NET Core -pohjainen yksikkötestiesimerkki että laajennettu aiempaa SQL-testisovellusta yksikkötesteillä.

---
+++ DotNet/SQL-testi/SqlServerTesti/SqlServerTestiTests/LaskentaTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SqlServerTesti;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlServerTesti.Tests
+{
+    [TestClass()]
+    public class LaskentaTests
+    {
+        [TestMethod()]
+        public void SummaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
