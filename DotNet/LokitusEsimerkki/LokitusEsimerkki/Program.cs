0e3c7e54bebca7b17174dbeef1d90fd5ca0edbcb
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-03-24 13:08:45 +0200

    Aloitettu lokitusesimerkin koodaus C#:lla.

---
+++ DotNet/LokitusEsimerkki/LokitusEsimerkki/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LokitusEsimerkki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
