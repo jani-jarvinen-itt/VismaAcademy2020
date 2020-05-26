1c6ac53675411daa5fe9a0247a3edf0e0f11f44c
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-05-26 13:34:03 +0300

    Jatkettu API-kontrollerin testaamista.

--- DotNet/WebApi/AspNetWebApiTest/AspNetWebApiTest/Controllers/TestiApiController.cs
+++ DotNet/WebApi/AspNetWebApiTest/AspNetWebApiTest/Controllers/TestiApiController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetWebApiTest.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,60 @@ namespace AspNetWebApiTest.Controllers
     [ApiController]
     public class TestiApiController : ControllerBase
     {
+        [Route("bool")]
+        public bool KylläEi()
+        {
+            return true;
+        }
+
+        [Route("datetime")]
+        public DateTime Nyt()
+        {
+            return DateTime.Now;
+        }
+
+        [Route("class")]
+        public Asiakas Asiakas()
+        {
+            return new Asiakas()
+            {
+                Id = 1200,
+                Nimi = "Yritys Oy",
+                Puhelin = "019 200 480"
+            };
+        }
+
+
+        /*
+        [Route("parametrit/{id:int}")]
+        public string ParametriTesti(int id)
+        {
+            return "Pyydetty luku-id: " + id;
+        }
+
+        [Route("parametrit/{id:alpha}")]
+        public string ParametriTesti2(string id)
+        {
+            return "Pyydetty merkkijono-id: " + id;
+        }
+
+        [Route("kokeilu/{id}")]
+        [HttpPost]
+        public string ParametriKokeilu(string id)
+        {
+            bool isNumber = int.TryParse(id, out int idValue);
+            if (isNumber)
+            {
+                return "Id-parametri on luku: " + idValue;
+            }
+            else
+            {
+                return "Id-parametri on merkkijono: " + id;
+            }
+        }
+        */
+
+
         /*
         [HttpGet]
         [HttpPost]
@@ -27,6 +82,7 @@ namespace AspNetWebApiTest.Controllers
         }
         */
 
+        /*
         [Route("Metodi1")]
         [HttpPut]
         public string Metodi1()
@@ -46,5 +102,20 @@ namespace AspNetWebApiTest.Controllers
         {
             return "Tämä on metodi 3";
         }
+        */
+
+        /*
+        [Route("ääkkönen")]
+        public string Ääkköstesti()
+        {
+            return "Tämä on ääkköstesti-metodi 1";
+        }
+
+        [Route("Päällekkäinen")]
+        public string Päällekkäinen2()
+        {
+            return "Tämä on päällekkäinen metodi 2";
+        }
+        */
     }
 }
---
+++ DotNet/WebApi/AspNetWebApiTest/AspNetWebApiTest/Models/Asiakas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetWebApiTest.Models
+{
+    public class Asiakas
+    {
+        public int Id { get; set; }
+        
+        public string Nimi { get; set; }
+
+        public string Puhelin { get; set; }
+    }
+}
