bf1f2deed5b1b42bd879230aa5b4e37aebfef1d1
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-05-26 11:15:30 +0300

    Lisätty uusi API-kontrolleri, jolla voidaan testata sovellusta.

---
+++ DotNet/WebApi/AspNetWebApiTest/AspNetWebApiTest/Controllers/TestiApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetWebApiTest.Controllers
+{
+    [Route("api/testi")]
+    [ApiController]
+    public class TestiApiController : ControllerBase
+    {
+        [HttpGet]
+        [HttpPost]
+        public string Hello()
+        {
+            return "Moi ASP.NET Web API:sta!";
+        }
+
+        [HttpPut]
+        public string Hello2()
+        {
+            return "Toinen versio metodista";
+        }
+    }
+}
