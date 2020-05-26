fc9707f67034913728a3a2a6f41cb280a35087bc
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-05-26 10:34:20 +0300

    Lisätty C#-esimerkki ADO.NET-kirjaston käytöstä.

---
+++ DotNet/AdoNetDemo/AdoNetDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AdoNetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aloitetaan tietokannan käsittely.");
+
+            string connStr = @"Server=localhost\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
+            SqlConnection connection = new SqlConnection(connStr);
+            SqlCommand cmd = null;
+            SqlDataReader reader = null;
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Tietokantayhteys avauttu.");
+
+                /*
+                SqlTransaction tran = connection.BeginTransaction();
+                tran.Commit();
+                tran.Rollback();
+                */
+
+                // luetaan asiakkaat tietokannasta
+                string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+                cmd = new SqlCommand(sql, connection);
+                reader = cmd.ExecuteReader();
+                Console.WriteLine("Kysely suoritettu.");
+
+                // tulostetaan asiakastiedot ruudule
+                Console.WriteLine();
+                while (reader.Read())
+                {
+                    string company = reader["CompanyName"].ToString();
+                    Console.WriteLine("Löytyi asiakas: " + company);
+                }
+                Console.WriteLine();
+            }
+            finally
+            {
+                /*
+                if (reader != null) reader.Close();
+                if (cmd != null) cmd.Dispose();
+                */
+                reader?.Close();
+                cmd?.Dispose();
+
+                Console.WriteLine("Suljetaan tietokantayhteys.");
+                connection.Close();
+            }
+
+            Console.WriteLine("Sovelluksen suoritus päättyy.");
+            Console.ReadLine();
+        }
+    }
+}
