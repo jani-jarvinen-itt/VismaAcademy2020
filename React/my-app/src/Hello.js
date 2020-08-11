fc651c2eb755d936124ad120db3447d4cb700650
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-11 14:55:12 +0300

    Lisätty Hello-komponentti React-sovellukseen.

---
+++ React/my-app/src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Hello() {
+  return (
+    <h1>Tämä on Hello-komponentti!</h1>
+  );
+}
+
+export default Hello;
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Hello from './Hello';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
