13f15b68ecbc0fe462d02db4ad8e55cab47420e5
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-11 10:19:51 +0300

    Lisätty ASP.NET Core MVC-sovellus versionhallintaan.

---
+++ DotNet/AspNetCoreBackendDemo/AspNetCoreBackendDemo/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBackendDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public int[] Luvut()
+        {
+            return new int[] { 1, 2, 3, 4 };
+        }
+    }
+}
