f1ac735d449151cc4c171c683c7d2ea7b4180b8a
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-11 10:42:53 +0300

    Lisätty AdventureWorks-tietokantaesimerkki.

--- DotNet/Integraatio/AdventureWorksDemo/AdventureWorksDemo/Form1.Designer.cs
+++ DotNet/Integraatio/AdventureWorksDemo/AdventureWorksDemo/Form1.Designer.cs
@@ -28,20 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Data Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logTextBox.Location = new System.Drawing.Point(13, 49);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(569, 400);
+            this.logTextBox.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(171, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(278, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(385, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(491, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "NW Insert Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(452, 169);
+            this.ClientSize = new System.Drawing.Size(594, 461);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- DotNet/Integraatio/AdventureWorksDemo/AdventureWorksDemo/Form1.cs
+++ DotNet/Integraatio/AdventureWorksDemo/AdventureWorksDemo/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AdventureWorksDemo.Database.Adventure;
+using AdventureWorksDemo.Database.Northwind;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,68 @@ namespace AdventureWorksDemo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void AddToLog(string message)
+        {
+            string line = $"{DateTime.Now.TimeOfDay}: {message}\r\n";
+            logTextBox.Text += line;
+            Application.DoEvents();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddToLog("Starting data copy.");
+
+            AdventureWorks2016CTP3Entities adventureContext = new AdventureWorks2016CTP3Entities();
+            NorthwindEntities northwindContext = new NorthwindEntities();
+
+            int rows = 0;
+            foreach (Database.Northwind.Customers nwCust in northwindContext.Customers)
+            {
+                string contact = nwCust.ContactName;
+                string[] nameParts = contact.Split(' ');
+                Database.Adventure.Person person = new Person()
+                {
+                    FirstName = nameParts[0],
+                    LastName = nameParts[1],
+                    // PersonPhone = nwCust.Phone
+                    // ...
+                };
+
+                AddToLog($"Processing customer: {nwCust.CompanyName}...");
+                adventureContext.Person.Add(person);
+                rows++;
+
+                if ((rows % 1000) == 0)
+                {
+                    //adventureContext.SaveChanges();
+                }
+            }
+
+            //adventureContext.SaveChanges();
+            AddToLog($"Data copy complete, {rows} rows copied.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AdventureWorks2016CTP3Entities adventureContext = new AdventureWorks2016CTP3Entities();
+
+            Vendor vendor = new Vendor
+            {
+                Name = "Uusi yritys Oy"
+            };
+
+            adventureContext.Vendor.Add(vendor);
+            adventureContext.SaveChanges();
+
+            /*
+            Store newestStore = (from s in adventureContext.Store
+                                 orderby s.BusinessEntityID descending
+                                 select s).FirstOrDefault();
+            */
+
+            MessageBox.Show("Vendor ID = " + vendor.BusinessEntityID);
         }
     }
 }
