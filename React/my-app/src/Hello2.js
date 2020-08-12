e18f9062615460110bfe56b4bc0ba830922bcca8
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-12 10:55:02 +0300

    Lisätty esimerkki JSX-syntaksista ja fragmentista.

--- React/my-app/src/Hello2.js
+++ React/my-app/src/Hello2.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Hello2 extends React.Component {
     render() {
-        return <h1>Moi {this.props.name}!</h1>;
+        return (
+        <>
+            <h1>Moi {this.props.name}!</h1>
+            <p>Moi moi!</p>
+        </>);
     }
 }
 
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import LifecycleDemo from './LifecycleDemo';
+import Hello2 from './Hello2';
 
 ReactDOM.render(
   <React.StrictMode>
     <LifecycleDemo />
+    <Hello2 />
   </React.StrictMode>,
   document.getElementById('root')
 );
